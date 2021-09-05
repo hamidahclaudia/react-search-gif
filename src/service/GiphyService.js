7753c693d343f71e0f76151beba047811c0b8e11
hamidahclaudia <claudiahamidah@gmail.com>
2021-09-05 16:23:54 +0700

    Fix api url

--- src/service/GiphyService.js
+++ src/service/GiphyService.js
@@ -1,5 +1,5 @@
 export const getGifByKeyword = async (keyword, limit) => {
-  const res = await fetch(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=e2Ud7Rflq9j5Jnq58CWWRVfmf1Z5thCx&limit=${limit}`)
+  const res = await fetch(`https://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=e2Ud7Rflq9j5Jnq58CWWRVfmf1Z5thCx&limit=${limit}`)
 
   const data = await res.json();
 
